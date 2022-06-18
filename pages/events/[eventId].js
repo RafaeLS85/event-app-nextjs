f0377c2f26afa0c01fa83ee9dae4fbbb928cde0d
Rafael Olivay <rafaelsebastian@live.com.ar>
2022-06-17 21:07:45 -0300

    add event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,27 @@
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+import EventContent from '../../components/event-detail/event-content'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventSummary from '../../components/event-detail/event-summary'
+import { getEventById } from '../../data/dummy-data'
 
 function EventDetailPage(){
     const router = useRouter()
-    const {eventId} = router.query
-    return <h1>Events detail page: {eventId}</h1>
+    const { eventId } = router.query
+    const event = getEventById(eventId)
+
+    if(!event){
+        return <p>No event found</p>
+    }
+
+    return (
+        <>
+           <EventSummary title={event.title} />
+           <EventLogistics event={event} />
+           <EventContent>
+            <p>{event.description}</p>
+           </EventContent>
+        </>
+    )
 }
 
 export default EventDetailPage
